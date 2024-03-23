5ff276337babd1708f2b1ea61575526b7b549645
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-03-23 13:11:13 +0100

    create data class which maps json file

---
+++ Launcher/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher
+{
+    public class Data
+    {
+        public string Operator { get; set; }
+        public double Value1 { get; set; }
+        public double Value2 { get; set; }
+        
+    }
+}
