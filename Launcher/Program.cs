a87b97217c02a8e1dca7e598f5d13f7967e36b0f
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-03-23 14:36:53 +0100

    add comments

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -6,10 +6,10 @@ namespace Launcher
     {
         static void Main(string[] args)
         {
-            List<double> values = new List<double>();
+            List<double> values = new List<double>(); //stores the objects mathematical results
             string json = File.ReadAllText("C:\\Users\\bigie\\source\\repos\\JsonCalculator\\Launcher\\input.json");
 
-            var data = JsonConvert.DeserializeObject<Dictionary<string,Data>>(json);
+            var data = JsonConvert.DeserializeObject<Dictionary<string,Data>>(json); //deserializes file into dictionary object
 
             double result = 0;
             int counter = 1;
@@ -29,10 +29,13 @@ namespace Launcher
                         result = x.Value.Value1 * x.Value.Value2;
                         break;
                     case "div":
-                        result = x.Value.Value1 / x.Value.Value2;
+                        if (x.Value.Value2 == 0)
+                            result = double.NaN;
+                        else
+                            result = x.Value.Value1 / x.Value.Value2;
                         break;
                     case "sqrt":
-                        result = (int)Math.Sqrt(x.Value.Value1);
+                        result = Math.Round( Math.Sqrt(x.Value.Value1),3);
                         break;
              }   
                 
@@ -40,6 +43,7 @@ namespace Launcher
                 counter++;
             }
 
+            //creates list of pairs
             var pairs = data.Select(x => new { ObjName = x.Value.ObjName, Value = values[data.Keys.ToList().IndexOf(x.Key)] });
 
             var sortedPairs = pairs.OrderBy(x => x.Value);
@@ -50,8 +54,9 @@ namespace Launcher
                 resultList += $"{pair.ObjName}: {pair.Value}\n";
             }
 
-            string resultFilePath = "C:\\Users\\bigie\\source\\repos\\JsonCalculator\\Launcher\\output.txt";
-            File.WriteAllText(resultFilePath, resultList);
+            string resultFilePath = "C:\\Users\\bigie\\source\\repos\\JsonCalculator\\Launcher\\output.txt"; //path for output
+            //File.WriteAllText(resultFilePath, resultList); Overrides the file
+            File.AppendAllText(resultFilePath, resultList);  //Append result to the file
 
             Console.WriteLine("Result successfully saved. \n");
 
